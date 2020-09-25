5733cb3914c67a0ee02a9aaad1a267da2134d75f
Daniel Lundin <erik.rikard.daniel@gmail.com>
2020-09-25 17:09:50 +0200

    Default to Docker for testing

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-rm -r work/ .nextflow.log* ; nextflow run main.nf -with-singularity pfitmap-nextflow.simg -profile test
+#rm -r work/ .nextflow.log* ; nextflow run main.nf -with-singularity pfitmap-nextflow.simg -profile test
+rm -fr work/ .nextflow.log* ; nextflow run main.nf -with-docker pfitmap-nextflow-image -profile test
