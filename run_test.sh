5d6e79439486dcb433c86a7950b2a2bcd2e84ad5
Daniel Lundin <erik.rikard.daniel@gmail.com>
2020-09-28 18:23:51 +0200

    ...

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 #rm -r work/ .nextflow.log* ; nextflow run main.nf -with-singularity pfitmap-nextflow.simg -profile test
-rm -fr work/ .nextflow.log* ; nextflow run main.nf -with-docker pfitmap-nextflow-image -profile test
+rm -fr test_results/* work/ .nextflow.log* ; nextflow run main.nf -with-docker pfitmap-nextflow-image -profile test
